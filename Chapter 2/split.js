50d269c37f522cf9e4f5677edb1d1f76e792e501
mhafet17 <do.crazy192@gmail.com>
2020-06-09 22:01:18 +0700

    menambah file split.js di Chapter 2

---
+++ Chapter 2/split.js
@@ -0,0 +1,9 @@
+let nama = "Gita Savitri";
+console.log(nama.split(" "));
+// hasil: ["Gita", "Savitri"]
+let firstName = "Gita";
+console.log(firstName.split(""));
+// hasil: ["G", "i", "t", "a"]
+let freinds = "Gita, Anis, Dian, Windy, Ina";
+console.log(freinds.split(","));
+// hasil: ["Gita", "Anis", "Dian", "LWindy", "Ina"]

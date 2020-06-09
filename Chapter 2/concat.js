ec0e4eb385994a3bec2f75ce88deb7edac1196ee
mhafet17 <do.crazy192@gmail.com>
2020-06-09 22:00:18 +0700

    menambah file concat.js di Chapter 2

---
+++ Chapter 2/concat.js
@@ -0,0 +1,17 @@
+let nama = "Madara Uchiha";
+let alamat = "JL.Shinju 09 Konoha";
+let pekerjaan = "Full Stack Javascript Developer";
+
+console.log(nama);
+console.log(alamat);
+console.log(pekerjaan);
+
+// concat string
+
+let namaDepan = "Maudy ";
+let namaBelakang = "Ayunda";
+
+// concat biasa (+)
+console.log("Nama Saya adalah: " + namaDepan + namaBelakang);
+// menggunakan template literal
+console.log(`Nama saya adalah: ${namaDepan + namaBelakang}`);

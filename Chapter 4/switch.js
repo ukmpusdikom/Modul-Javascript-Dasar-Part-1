dd9d78c3e2be78ef62d715233e7684de5a9d451d
mhafet17 <do.crazy192@gmail.com>
2020-06-12 18:27:12 +0700

    add switch.js

---
+++ Chapter 4/switch.js
@@ -0,0 +1,15 @@
+let cuaca = "berawan";
+switch (cuaca) {
+  case "hujan":
+    console.log("Kamu butuh jas hujan");
+    break;
+  case "berawan":
+    console.log("Sedikit dingin, gunakan jaket kesayanganmu");
+    break;
+  case "cerah":
+    console.log("Senangnya bisa kemana saja");
+    break;
+  default:
+    console.log("BMKG belum update cuaca nih :(");
+}
+// hasil: Sedikit dingin, gunakan jaket kesayanganmu

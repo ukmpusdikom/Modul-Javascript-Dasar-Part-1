df605c467e791eaaf3efda7d17e0e64f34d96773
mhafet17 <do.crazy192@gmail.com>
2020-06-12 18:27:01 +0700

    add if.js

---
+++ Chapter 4/IF.js
@@ -0,0 +1,31 @@
+let usia = 19;
+if (usia >= 18) {
+  console.log("Okay selamat, Anda boleh ikut");
+}
+// hasil: Okay selamat, Anda boleh ikut
+
+if (syarat) {
+  // Jalankan kode jika syarat terpenuhi atau bernilai benar
+} else {
+  // Jalankan kode jika syarat tidak terpenuhi atau bernilai salah
+}
+
+let umur = 10;
+if (umur >= 18) {
+  console.log("Okay selamat, Anda boleh ikut");
+} else {
+  console.log("Maaf nak, Anda harus menunggu beberapa tahun lagi");
+}
+// hasil: Maaf nak, Anda harus menunggu beberapa tahun lagi
+
+let angka = 0;
+if (angka > 0) {
+  console.log(`${angka} adalah bilangan positif`);
+} else if (angka < 0) {
+  console.log(`${angka} adalah bilangan negati`);
+} else if (angka == 0) {
+  console.log(`${angka} adalah Nol`);
+} else {
+  console.log(`${angka} bukanlah angka yang valid`);
+}
+// hasil: 0 adalah Nol
